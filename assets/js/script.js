c3a4d13d3d8fb9217578350925465d8c985f46e0
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-03 22:36:27 -0500

    add function to fetch lat and lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,33 @@
-var requestURL = "https://api.github.com/users/corasinth"
+// =============== Variables ===============
+var apiKey = 
+var cityQuery = "City of London"
+var latitude
+var longitude 
 
-var data
-function fetchRequest () {
-    fetch (requestURL)
+var geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${apiKey}`
+var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+
+
+// =============== Functions ===============
+function fetchLatLon () {
+    fetch (geocodeURL)
     .then(function(response) {
-    data = response.json();
-    console.log (data)
-    }
-    )
+    return response.json();
+    })
+    .then (function (geocodeInfo) {
+    console.log (geocodeInfo)
+    console.log(geocodeInfo[0].lat);
+    console.log(geocodeInfo[0].lon);
+    })
 }
 
-
+function fetchWeather () {
+    fetch (requestURL)
+    .then(function(response) {
+    return response.json();
+    })
+    .then (function (weatherInfo) {
+    
+    console.log (weatherInfo)
+    })
+}
