b5a68866a81fe4276611a35516ff4b9f6ad958c5
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-03 23:12:42 -0500

    add fetch weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 // =============== Variables ===============
 var apiKey = 
 var cityQuery = "City of London"
-var latitude
+var latitude 
 var longitude 
-
+var weatherURL
 var geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${apiKey}`
-var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
-
 
 // =============== Functions ===============
 function fetchLatLon () {
@@ -15,19 +13,27 @@ function fetchLatLon () {
     return response.json();
     })
     .then (function (geocodeInfo) {
-    console.log (geocodeInfo)
-    console.log(geocodeInfo[0].lat);
-    console.log(geocodeInfo[0].lon);
-    })
+    latitude = geocodeInfo[0].lat;
+    longitude = geocodeInfo[0].lon;
+})
 }
 
 function fetchWeather () {
-    fetch (requestURL)
+    weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
+    fetch (weatherURL)
     .then(function(response) {
     return response.json();
     })
     .then (function (weatherInfo) {
-    
-    console.log (weatherInfo)
+    console.log(weatherURL);
+    console.log (weatherInfo);
+    console.log(weatherInfo.current.temp);
+    console.log(weatherInfo.current.humidity);
+    console.log(weatherInfo.current.wind_speed);
+    console.log(weatherInfo.current.uvi);
+    console.log(weatherInfo.current.weather[0].icon);
+    console.log(weatherInfo.daily[0].temp.day);
+    console.log(weatherInfo.daily[0].humidity);
+    console.log(weatherInfo.daily[0].weather[0].icon);
     })
 }
