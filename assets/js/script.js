c017857162346025ae53f52f86191050bb8ef4b7
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-04 03:07:17 -0500

    get search box to return appropriate values in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,31 @@
 // =============== Variables ===============
 var submitButton = document.getElementById("submit")
-var apiKey
+var apiKey = 
 var latitude 
 var longitude 
 var weatherURL
 var geocodeURL
 
 // =============== Functions for Getting Data ===============
-function fetchLatLon (cityQuery) {
-    geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityQuery}&limit=5&appid=${apiKey}`
+function fetchLatLon (city) {
+    geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
     fetch (geocodeURL)
     .then(function(response) {
     return response.json();
     })
     .then (function (geocodeInfo) {
-    latitude = geocodeInfo[0].lat;
-    longitude = geocodeInfo[0].lon;
+    latitude = geocodeInfo[0].lat
+    longitude = geocodeInfo[0].lon
+    return [geocodeInfo[0].lat, geocodeInfo[0].lon]
 })
+.then (function check () {
+    console.log ([latitude, longitude])
+    fetchWeather([latitude, longitude])
 }
+)}
 
-function fetchWeather () {
-    weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
+function fetchWeather (coordinateArray) {
+    weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinateArray[0]}&lon=${coordinateArray[1]}&units=imperial&appid=${apiKey}`
     fetch (weatherURL)
     .then(function(response) {
     return response.json();
@@ -44,9 +49,10 @@ function fetchWeather () {
 
 // =============== Functions for Generating Elements on Page ===============
 submitButton.addEventListener("click", function (event) {
-    event.preventDefault()
-    var citySearchValue = document.getElementById("input").value
-    console.log(citySearchValue)
+    event.preventDefault();
+    var citySearchValue = document.getElementById("input").value;
+    console.log("this is" + citySearchValue);
+     fetchLatLon(citySearchValue);
 })
 
 // =============== Functions for Storing and Retrieving Data from Local Storage ===============
