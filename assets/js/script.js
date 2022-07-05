2568a0f04a8f732fb7608c9bf4fc642fff5981db
Corasinth <beforeyouknowkindness@gmail.com>
2022-07-05 01:09:08 -0500

    write current weather information to page on search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
 // =============== Variables ===============
-var submitButton = document.getElementById("submit")
-var apiKey
+var submitButton = document.getElementById("submit") 
+var nameOfCity
 var weatherURL
 var geocodeURL
 var keyList = []
+
+var cityEl = document.getElementById("city")
+var temperature = document.getElementById("temperature");
+var humidity = document.getElementById("humidity")
+var windSpeed = document.getElementById("windSpeed")
+var uvIndex = document.getElementById("uvIndex")
+
 // =============== Functions for Getting and Storing API Data ===============
 
 //Fetches latitude and longitude from API, saves them to local storage along with the appropriate key names, and gives fetchWeather the coordinates
@@ -14,31 +21,28 @@ function fetchLatLon (city) {
     return response.json();
     })
     .then (function (geocodeInfo) {
-    //Although location is defined just 5 lines lower, for some reason if it isn't defined 
     var latitude = geocodeInfo[0].lat
     var longitude = geocodeInfo[0].lon 
-    //Although location is defined in the if statements below, if it is not defined here then the key list includes a strange object for no apparent reason. If the var location isn't created here, then the HTML file gets saved to local storage, and the tab crashes until the item is cleared. While this can be solved by simply not including the if statements below and letting the key list read out with "undefined" strings, The current implementation does work without any bugs I've been able to find. 
-    var location = `${geocodeInfo[0].name}, ${geocodeInfo[0].state}, ${geocodeInfo[0].country}`   
-   //This keeps undefineds from being part of the key names
-    if (Boolean(geocodeInfo[0].state) === false) {
-        location = `${geocodeInfo[0].name}, ${geocodeInfo[0].country}`
+   //This keeps undefineds from being part of the key names and prevents duplicates
+    if (Boolean(geocodeInfo[0].state) === false || geocodeInfo[0].state === geocodeInfo[0].name) {
+        nameOfCity = `${geocodeInfo[0].name}, ${geocodeInfo[0].country}`
     } else {
-        location = `${geocodeInfo[0].name}, ${geocodeInfo[0].state}, ${geocodeInfo[0].country}`
+        nameOfCity = `${geocodeInfo[0].name}, ${geocodeInfo[0].state}, ${geocodeInfo[0].country}`
     }
     //Prevents duplicates from clogging search history
-    if (keyList.includes(location) === false) {
+    if (keyList.includes(nameOfCity) === false) {
         //Keeps search history to 10 items or less
         if (keyList.length === 10) {
             localStorage.removeItem(keyList[0])
             keyList.shift()
-            keyList.push (location)
+            keyList.push (nameOfCity)
             localStorage.setItem ("keyList", JSON.stringify(keyList))
         } else {
-        keyList.push (location)
+        keyList.push (nameOfCity)
         localStorage.setItem ("keyList", JSON.stringify(keyList))
         }
     }
-    localStorage.setItem (location, JSON.stringify([latitude, longitude]))
+    localStorage.setItem (nameOfCity, JSON.stringify([latitude, longitude]))
     fetchWeather([latitude, longitude ])
 })
 }
@@ -51,14 +55,17 @@ function fetchWeather (coordinateArray) {
     return response.json();
     })
     .then (function (weatherInfo) {
+    //Takes a variety of information from the returned data and writes it to appropriate places in the HTML
     console.log(weatherURL);
     console.log (weatherInfo);
     console.log(new Date(weatherInfo.current.dt*1000).toDateString());
-    console.log(weatherInfo.current.temp);
-    console.log(weatherInfo.current.humidity);
-    console.log(weatherInfo.current.wind_speed);
-    console.log(weatherInfo.current.uvi);
-    console.log(weatherInfo.current.weather[0].icon);
+    cityEl.textContent = `${nameOfCity} (${new Date(weatherInfo.current.dt*1000).toDateString()})`;
+    var link = `https://openweathermap.org/img/wn/${weatherInfo.current.weather[0].icon}.png`
+    document.getElementById("currentWeatherIcon").setAttribute("src", link);
+    temperature.textContent = weatherInfo.current.temp;
+    humidity.textContent = weatherInfo.current.humidity;
+    windSpeed.textContent = weatherInfo.current.wind_speed;
+    uvIndex.textContent = weatherInfo.current.uvi;
     //index 4-9 for month day format
     console.log(new Date(weatherInfo.daily[0].dt*1000).toDateString());
     console.log(weatherInfo.daily[0].temp.day);
@@ -87,7 +94,6 @@ submitButton.addEventListener("click", function (event) {
     event.preventDefault();
     var citySearchValue = document.getElementById("input").value;
     document.getElementById("input").value = "";
-    console.log("this is" + citySearchValue);
     fetchLatLon(citySearchValue);
 })
 
